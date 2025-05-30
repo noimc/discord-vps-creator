db00b7612fa6f40f4b7f2a2cdd0833891eb70908
Noi MC <yutynetwork2011@gmail.com>
2025-05-30 16:26:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,14 +36,14 @@ echo "Building Docker Image"
 sudo docker build -t ubuntu-22.04-with-tmate .
 echo Built successfully
 echo "Downloading main.py from the GitHub repository..."
-wget -O main.py https://raw.githubusercontent.com/katy-the-kat/discord-vps-creator/refs/heads/main/v3ds
+wget -O main.py https://raw.githubusercontent.com/katy-the-kat/discord-vps-creator/refs/heads/main/v2d
 echo Downloaded successfully
 echo "Installing Python packages: discord and docker..."
 pip3 install discord docker
 echo "Please enter your Discord bot token, Make a bot at discord.dev and get the token, You dont need any intents:"
 read -r DISCORD_TOKEN
 echo "Updating main.py with the provided Discord token..."
-sed -i "s/TOKEN = ''/TOKEN = '$DISCORD_TOKEN'/" main.py
+sed -i "s/TOKEN = ''/TOKEN = '$DISCORD_TOKEN'/" v2d
 echo "Starting the Discord bot..."
-echo "To start the bot in the future, run: python3 main.py"
-python3 main.py
+echo "To start the bot in the future, run: python3 v2d"
+python3 v2d
